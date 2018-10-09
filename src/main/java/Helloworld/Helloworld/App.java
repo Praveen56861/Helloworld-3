24b5723a6ac8b36290ce933003316d3cc6c001b0
United TCS <unitedtcs@Uniteds-Mac-mini.local>
2018-10-09 12:08:35 +0530

    first

---
+++ src/main/java/Helloworld/Helloworld/App.java
@@ -0,0 +1,13 @@
+package Helloworld.Helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
